53419e7e57e1b14623b6c903d3f2daea08c509ae
mochfahrizalzahir-Unitomo <mochfahrizalzahir@gmail.com>
2026-01-06 21:32:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+from sympy import sympify, symbols, lambdify
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Input & Parsing")
+
+fungsi_str = st.sidebar.text_input("Masukkan Fungsi f(x):", value="x^3 - x - 2")
+x_sym = symbols('x')
+
+try:
+    expr = sympify(fungsi_str)
+    f = lambdify(x_sym, expr, 'numpy')
+    
+    st.write(f"Fungsi yang terdeteksi: **f(x) = {expr}**")
+    test_val = st.number_input("Coba masukkan nilai x untuk cek f(x):", value=1.0)
+    st.write(f"Hasil f({test_val}) = {f(test_val)}")
+except Exception as e:
+    st.error(f"Input error: {e}")
