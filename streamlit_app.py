af0fd1c1c0c588d747083ed4e6275c7ca757ed39
mochfahrizalzahir-Unitomo <mochfahrizalzahir@gmail.com>
2026-01-20 16:21:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,45 @@ try:
     st.write(f"Hasil f({test_val}) = {f(test_val)}")
 except Exception as e:
     st.error(f"Input error: {e}")
+a = st.sidebar.number_input("Batas Bawah (a):", value=1.0)
+b = st.sidebar.number_input("Batas Atas (b):", value=2.0)
+
+if st.sidebar.button("Jalankan Bisection"):
+    fa, fb = f(a), f(b)
+    
+    if fa * fb >= 0:
+        st.error("Syarat f(a)*f(b) < 0 tidak terpenuhi. Akar mungkin tidak ada di rentang ini.")
+    else:
+        c = (a + b) / 2
+        st.success(f"Iterasi 1 berhasil: Titik tengah awal adalah {c}")
+        import pandas as pd
+
+data_iterasi = []
+tol = 0.001
+for i in range(1, 11): 
+    c = (a + b) / 2
+    fc = f(c)
+    data_iterasi.append({"Iterasi": i, "a": a, "b": b, "c": c, "f(c)": fc})
+    
+    if abs(fc) < 1e-15 or abs(b-a) < tol:
+        break
+    if f(a) * fc < 0:
+        b = c
+    else:
+        a = c
+
+df = pd.DataFrame(data_iterasi)
+st.table(df)
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.subheader("Visualisasi Grafik")
+x_vals = np.linspace(a - 1, b + 1, 100)
+y_vals = f(x_vals)
+
+fig, ax = plt.subplots()
+ax.plot(x_vals, y_vals, label="f(x)")
+ax.axhline(0, color='black', linewidth=1) # Garis x=0
+ax.scatter([c], [f(c)], color='red', label='Akar') # Titik Akar
+ax.legend()
+st.pyplot(fig)
